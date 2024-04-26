c54701ed8adbc58f9da6e2c37c5b85a4fca95db4
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-26 11:11:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -756,17 +756,22 @@ const cp = require('child_process')
 const {promisify} = require('util') 
 const exec = promisify(cp.exec).bind(cp);
 let o;
-conn.fakeReply(m.chat, `🚀 𝐏𝐫𝐮𝐞𝐧𝐚 𝐝𝐞 𝐯𝐞𝐥𝐨𝐜𝐢𝐝𝐚𝐝`, '0@s.whatsapp.net', 'test')
+conn.fakeReply(m.chat, `🚀 𝐏𝐫𝐮𝐞𝐛𝐚 𝐝𝐞 𝐯𝐞𝐥𝐨𝐜𝐢𝐝𝐚𝐝`, '0@s.whatsapp.net', 'test')
 try {
-o = await exec('python3 speed.py --secure --share');
-} catch (e) {
-o = e;
-} finally {
+o = await exec('python3 ookla-speedtest.py --secure --share');
 const {stdout, stderr} = o;
-if (stdout.trim()) m.reply(stdout);
-if (stderr.trim()) m.reply(stderr)
-console.log(stderr) 
-}}
+if (stdout.trim()) {
+const match = stdout.match(/http[^"]+\.png/);
+const urlImagen = match ? match[0] : null;
+await conn.sendMessage(m.chat, {image: {url: urlImagen}, caption: stdout.trim()}, {quoted: m})}
+if (stderr.trim()) { 
+const match2 = stderr.match(/http[^"]+\.png/);
+const urlImagen2 = match2 ? match2[0] : null;    
+await conn.sendMessage(m.chat, {image: {url: urlImagen2}, caption: stderr.trim()}, {quoted: m});
+}} catch (e) {
+o = e.message;
+return m.reply(o)
+console.log(e)}}
 break
 
 case 'setrules': case 'addrules': case 'addrule': {
