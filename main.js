e34c98cc2a96bf1ba3941dea528d75c647db37eb
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-05-01 16:21:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -783,8 +783,27 @@ break
 
 case 'reglas': case 'rule': case 'rules': {
 let chat = global.db.data.chats[m.chat]
-if (!chat.rules === '') m.reply(`Sin reglas`) 
-m.reply(`*${chat.rules}`)}
+if (!chat.rules === '') m.reply(`*⚠️ Reglas de nuestro hosting:* 
+
+_*🔰 Servidores gratuitos:*_
+
+• No pueden vender ni comercializar ningún servidor gratuito nuestro.
+• Se brinda mantenimiento a nuestros sistemas, pero no siempre será perfecto. Los problemas con el servicio gratuito serán atendidos cuando sea posible. No se aceptan exigencias ni quejas.
+• No nos hacemos responsables de problemas con la configuración de su servidor.
+• No prometemos un tiempo de actividad del 100%. Sin embargo, estaremos pendientes para solucionar cualquier problema.
+• Cualquier servidor que utilice cantidades irrazonables o sin sentido de almacenamiento (cosas absurdas que no tengan relación con el servidor) puede ser eliminado.
+• No nos hacemos responsables por la pérdida de datos. Siempre recomendamos a nuestros usuarios hacer copias de seguridad.
+• En caso de que una máquina deje de funcionar, no proporcionamos ni tenemos copias de seguridad.
+• Cualquier servidor ubicado en la capa gratuita e inactivo durante 7 días o más podrá ser eliminado sin aviso previo y sin derecho a reclamaciones por parte del usuario.
+
+*👑 Servidores Premium:*
+
+• Soporte y Responsabilidad del Hosting: Brindamos soporte para cualquier problema relacionado con nuestro servicio de hosting. Sin embargo, no asumimos responsabilidad por la creación de servidores. Nos comprometemos a alojar su servidor de la mejor manera posible.
+• Pagos y Suspensión del Servicio: Si no realiza el pago de su servicio, este será suspendido. Para reactivarlo, deberá adquirir InyCoins a través de nuestro método de pago.
+• Devoluciones de Cargo y Transacciones Revertidas: Cualquier devolución de cargo o transacción revertida en sus pagos resultará en la cancelación inmediata de su cuenta. Esto conllevará a la prohibición del uso de nuestros servicios en el futuro, además de la pérdida del dinero involucrado.
+• Uso y Reventa de Servicios: Nuestros servicios no pueden revenderse ni comercializarse de ninguna manera, a menos que cuente con el plan de reventa, también conocido como reseller. En caso contrario, los servidores pueden suspenderse y/o eliminarse sin previo aviso.
+• Eliminación de Servidores no Pagados: Si el pago del servidor no se realiza después de 7 días de su suspensión, este puede ser eliminado sin previo aviso, lo que resultará en la pérdida de los datos asociados al servidor.`) 
+m.reply(`${chat.rules}`)}
 break		
 		
 case 'grupo':
